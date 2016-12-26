d990ea32637b2663c8eaff82e49accfcc50f2c91
tamal <tamal@appscode.com>
2016-12-26 07:55:22 -0800

    Fix common_test

--- pkg/runtime/common_test.go
+++ pkg/runtime/common_test.go
@@ -1,31 +1,11 @@
-// common_test.go
 package runtime
 
 import (
-	"strings"
 	"testing"
 )
 
-func TestCmdDescription(t *testing.T) {
-	if strings.Index(CmdDescription(PT_SubmitJobEpoch+1), "unknown") != 0 {
-		t.Error("index should be 0")
-	}
-
-	if strings.Index(CmdDescription(PT_CanDo), "CAN_DO") != 0 {
-		t.Error("index should be 0")
-	}
-
-	if strings.Index(CmdDescription(PT_SubmitJobEpoch), "SUBMIT_JOB_EPOCH") != 0 {
-		t.Error("index should be 0")
-	}
-}
-
 func TestCmdArgCount(t *testing.T) {
-	if ArgCount(PT_UnUNUSED) != 0 {
-		t.Error("argument count not match")
-	}
-
-	if ArgCount(PT_SubmitJobEpoch) != 4 {
+	if PT_SubmitJobEpoch.ArgCount() != 4 {
 		t.Error("argument count not match")
 	}
 }
