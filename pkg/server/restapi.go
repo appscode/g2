f0806174d46923a049d3849ef4894990e4cafc16
tamal <tamal@appscode.com>
2016-12-26 17:10:10 -0800

    Remove dependency on martini.

--- pkg/server/restapi.go
+++ pkg/server/restapi.go
@@ -6,23 +6,22 @@ import (
 
 	"github.com/appscode/log"
 	"github.com/go-macaron/toolbox"
-	"github.com/go-martini/martini"
 	"github.com/ngaut/stats"
 	"gopkg.in/macaron.v1"
 )
 
-func getJob(s *Server, params martini.Params) string {
+func getJob(s *Server, ctx *macaron.Context) string {
 	e := &event{tp: ctrlGetJob,
-		jobHandle: params["jobhandle"], result: createResCh()}
+		jobHandle: ctx.Params("jobhandle"), result: createResCh()}
 	s.ctrlEvtCh <- e
 	res := <-e.result
 
 	return res.(string)
 }
 
-func getWorker(s *Server, params martini.Params) string {
+func getWorker(s *Server, ctx *macaron.Context) string {
 	e := &event{tp: ctrlGetWorker,
-		args: &Tuple{t0: params["cando"]}, result: createResCh()}
+		args: &Tuple{t0: ctx.Params("cando")}, result: createResCh()}
 	s.ctrlEvtCh <- e
 	res := <-e.result
 
@@ -46,21 +45,21 @@ func registerWebHandler(s *Server) {
 	m.Use(toolbox.Toolboxer(m))
 	m.Get("/debug/stats", stats.ExpvarHandler)
 
-	m.Get("/job", func(params martini.Params) string {
-		return getJob(s, params)
+	m.Get("/job", func(ctx *macaron.Context) string {
+		return getJob(s, ctx)
 	})
 
 	//get job information using job handle
-	m.Get("/job/:jobhandle", func(params martini.Params) string {
-		return getJob(s, params)
+	m.Get("/job/:jobhandle", func(ctx *macaron.Context) string {
+		return getJob(s, ctx)
 	})
 
-	m.Get("/worker", func(params martini.Params) string {
-		return getWorker(s, params)
+	m.Get("/worker", func(ctx *macaron.Context) string {
+		return getWorker(s, ctx)
 	})
 
-	m.Get("/worker/:cando", func(params martini.Params) string {
-		return getWorker(s, params)
+	m.Get("/worker/:cando", func(ctx *macaron.Context) string {
+		return getWorker(s, ctx)
 	})
 
 	log.Fatal(http.ListenAndServe(addr, m))
