1f7b8477a97a2cb528b54de70b8d516df10e774f
Md. Ashiquzzaman <ashiquzzaman33@users.noreply.github.com>
2017-01-13 13:20:43 +0600

    Add cancel and prioritystatus admin protocol. (#5)
    * Add cancel and prioritystatus admin protocol.
    
    * fix
     - `cancel-job` instead of `cancel`
     -  Use `%v` where possible

--- gearadmin/gearadmin.go
+++ gearadmin/gearadmin.go
@@ -2,6 +2,7 @@ package gearadmin
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"io"
 	"strconv"
@@ -26,6 +27,14 @@ type Status struct {
 	AvailableWorkers int
 }
 
+type PriorityStatus struct {
+	Function         string
+	HighQueued       int
+	NormalQueued     int
+	LowQueued        int
+	AvailableWorkers int
+}
+
 // Worker represents a worker connected to gearman as returned by the "workers" command.
 type Worker struct {
 	Fd        string
@@ -42,19 +51,19 @@ func (ga GearmanAdmin) Status() ([]Status, error) {
 	for scanner.Scan() && scanner.Text() != "." {
 		toks := strings.Split(scanner.Text(), "\t")
 		if len(toks) != 4 {
-			return statuses, fmt.Errorf("unexpected status: '%s'", scanner.Text())
+			return statuses, fmt.Errorf("unexpected status: '%v'", scanner.Text())
 		}
 		total, err := strconv.Atoi(toks[1])
 		if err != nil {
-			return statuses, fmt.Errorf("could not parse total: '%s'", scanner.Text())
+			return statuses, fmt.Errorf("could not parse total: '%v'", scanner.Text())
 		}
 		running, err := strconv.Atoi(toks[2])
 		if err != nil {
-			return statuses, fmt.Errorf("could not parse running: '%s'", scanner.Text())
+			return statuses, fmt.Errorf("could not parse running: '%v'", scanner.Text())
 		}
 		available, err := strconv.Atoi(toks[3])
 		if err != nil {
-			return statuses, fmt.Errorf("could not parse available: '%s'", scanner.Text())
+			return statuses, fmt.Errorf("could not parse available: '%v'", scanner.Text())
 		}
 		statuses = append(statuses, Status{
 			Function:         toks[0],
@@ -66,6 +75,45 @@ func (ga GearmanAdmin) Status() ([]Status, error) {
 	return statuses, scanner.Err()
 }
 
+func (ga GearmanAdmin) PriorityStatus() ([]PriorityStatus, error) {
+	var pStatuses []PriorityStatus
+	fmt.Fprintf(ga.conn, "prioritystatus\n")
+	scanner := bufio.NewScanner(ga.conn)
+
+	for scanner.Scan() && scanner.Text() != "." {
+		toks := strings.Split(scanner.Text(), "\t")
+
+		if len(toks) != 5 {
+			return pStatuses, fmt.Errorf("unexpected prioritystatus: '%v'", scanner.Text())
+		}
+
+		hq, err := strconv.Atoi(toks[1])
+		if err != nil {
+			return pStatuses, fmt.Errorf("could not parse HIGH-QUEUED: '%v'", scanner.Text())
+		}
+		nq, err := strconv.Atoi(toks[2])
+		if err != nil {
+			return pStatuses, fmt.Errorf("could not parse NORMAL-QUEUED: '%v'", scanner.Text())
+		}
+		lq, err := strconv.Atoi(toks[3])
+		if err != nil {
+			return pStatuses, fmt.Errorf("could not parse LOW-QUEUED: '%v'", scanner.Text())
+		}
+		available, err := strconv.Atoi(toks[4])
+		if err != nil {
+			return pStatuses, fmt.Errorf("could not parse available: '%v'", scanner.Text())
+		}
+		pStatuses = append(pStatuses, PriorityStatus{
+			Function:         toks[0],
+			HighQueued:       hq,
+			NormalQueued:     nq,
+			LowQueued:        lq,
+			AvailableWorkers: available,
+		})
+	}
+	return pStatuses, scanner.Err()
+}
+
 // Workers returns a summary of workers connected to gearman.
 func (ga GearmanAdmin) Workers() ([]Worker, error) {
 	var workers []Worker
@@ -74,7 +122,7 @@ func (ga GearmanAdmin) Workers() ([]Worker, error) {
 	for scanner.Scan() && scanner.Text() != "." {
 		toks := strings.Split(scanner.Text(), " ")
 		if len(toks) < 4 {
-			return workers, fmt.Errorf("unexpected worker: '%s'", scanner.Text())
+			return workers, fmt.Errorf("unexpected worker: '%v'", scanner.Text())
 		}
 		workers = append(workers, Worker{
 			Fd:        toks[0],
@@ -84,4 +132,17 @@ func (ga GearmanAdmin) Workers() ([]Worker, error) {
 		})
 	}
 	return workers, scanner.Err()
+}
+
+func (ga GearmanAdmin) Cancel(handle string) (bool, error) {
+	fmt.Fprintf(ga.conn, fmt.Sprintf("cancel-job %v\n", handle))
+	scanner := bufio.NewScanner(ga.conn)
+	if scanner.Scan() {
+		resp := scanner.Text()
+		if resp == "OK" {
+			return true, nil
+		}
+		return false, errors.New(resp)
+	}
+	return false, scanner.Err()
 }
--- gearadmin/gearadmin_test.go
+++ gearadmin/gearadmin_test.go
@@ -45,20 +45,56 @@ fn2	0	1	2
 	}
 	checkStatus := func(status Status, fn string, total, running, available int) {
 		if status.Function != fn {
-			t.Fatalf("Incorrect function: expected '%s', got '%s'", fn, status.Function)
+			t.Fatalf("Incorrect function: expected '%v', got '%v'", fn, status.Function)
 		}
 		if status.Total != total {
-			t.Fatalf("Incorrect total: expected %d, got %d", total, status.Total)
+			t.Fatalf("Incorrect total: expected %v, got %v", total, status.Total)
 		}
 		if status.Running != running {
-			t.Fatalf("Incorrect running: expected %d, got %d", running, status.Running)
+			t.Fatalf("Incorrect running: expected %v, got %v", running, status.Running)
 		}
 		if status.AvailableWorkers != available {
-			t.Fatalf("Incorrect running: expected %d, got %d", available, status.AvailableWorkers)
+			t.Fatalf("Incorrect AVAILABLE_WORKERS: expected %v, got %v", available, status.AvailableWorkers)
 		}
 	}
 	checkStatus(statuses[0], "fn1", 3, 2, 1)
 	checkStatus(statuses[1], "fn2", 0, 1, 2)
+}
+
+func TestPriorityStatus(t *testing.T) {
+	mockGearmand := MockGearmand{}
+	mockGearmand.Responses = map[string]string{
+		"prioritystatus": `fn1	3	2	1	0
+fn2	0	1	2	0
+.`,
+	}
+	ga := GearmanAdmin{&mockGearmand}
+	pStatuses, err := ga.PriorityStatus()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(pStatuses) != 2 {
+		t.Fatalf("Expected two prioritystatuses")
+	}
+	checkPriorityStatus := func(pStatus PriorityStatus, fn string, hq, nq, lq, available int) {
+		if pStatus.Function != fn {
+			t.Fatalf("Incorrect function: expected '%v', got '%v'", fn, pStatus.Function)
+		}
+		if pStatus.HighQueued != hq {
+			t.Fatalf("Incorrect HIGH-QUEUED: expected %v, got %v", hq, pStatus.HighQueued)
+		}
+		if pStatus.NormalQueued != nq {
+			t.Fatalf("Incorrect NORMAL-QUEUED: expected %v, got %v", nq, pStatus.NormalQueued)
+		}
+		if pStatus.LowQueued != lq {
+			t.Fatalf("Incorrect LOW-QUEUED: expected %v, got %v", lq, pStatus.LowQueued)
+		}
+		if pStatus.AvailableWorkers != available {
+			t.Fatalf("Incorrect AVAILABLE_WORKERS: expected %v, got %v", available, pStatus.AvailableWorkers)
+		}
+	}
+	checkPriorityStatus(pStatuses[0], "fn1", 3, 2, 1, 0)
+	checkPriorityStatus(pStatuses[1], "fn2", 0, 1, 2, 0)
 }
 
 func TestWorkers(t *testing.T) {
@@ -79,13 +115,13 @@ func TestWorkers(t *testing.T) {
 	}
 	checkWorker := func(worker Worker, fd, ip, cid string, functions []string) {
 		if worker.Fd != fd {
-			t.Fatalf("Expected worker Fd '%s', got '%s'", fd, worker.Fd)
+			t.Fatalf("Expected worker Fd '%v', got '%v'", fd, worker.Fd)
 		}
 		if worker.IPAddress != ip {
-			t.Fatalf("Expected worker IPAddress '%s', got '%s'", ip, worker.IPAddress)
+			t.Fatalf("Expected worker IPAddress '%v', got '%v'", ip, worker.IPAddress)
 		}
 		if worker.ClientID != cid {
-			t.Fatalf("Expected worker ClientID '%s', got '%s'", cid, worker.ClientID)
+			t.Fatalf("Expected worker ClientID '%v', got '%v'", cid, worker.ClientID)
 		}
 		if fmt.Sprintf("%v", worker.Functions) != fmt.Sprintf("%v", functions) {
 			t.Fatalf("Expected worker Functions '%#v', got '%#v'", functions, worker.Functions)
@@ -94,4 +130,33 @@ func TestWorkers(t *testing.T) {
 	checkWorker(workers[0], "74", "10.0.1.167", "-", []string{})
 	checkWorker(workers[1], "284", "10.0.2.16", "-", []string{"fn1"})
 	checkWorker(workers[2], "284", "10.0.2.16", "-", []string{"fn1", "fn2"})
+}
+
+func TestCancel(t *testing.T) {
+	mockGearmand := MockGearmand{}
+	mockGearmand.Responses = map[string]string{}
+	handleResponsePair := []struct {
+		handle   string
+		success  bool
+		response string
+	}{
+		{"S:-icee:-17700-1483598255-1", true, "OK"},
+		{"S:-icee:-17700-1483598255-2", true, "OK"},
+		{"S:-icee:-17700-1483598255-5", false, "Error: handle `S:-icee:-17700-1483598255-5` not found"},
+		{"H:-icee:-17700-1483598255-5", false, "Error: Invalid handle `H:-icee:-17700-1483598255-5`, valid schedule job handle should start with `S:`"},
+		{"a-invalid-handle", false, "Error: Invalid handle `H:-icee:-17700-1483598255-5`, valid schedule job handle should start with `S:`"},
+	}
+	for _, o := range handleResponsePair {
+		mockGearmand.Responses[fmt.Sprintf("cancel-job %v", o.handle)] = o.response
+	}
+	ga := GearmanAdmin{&mockGearmand}
+	for _, v := range handleResponsePair {
+		isSuccess, err := ga.Cancel(v.handle)
+		if isSuccess != v.success {
+			t.Fatalf("Expected response '%v', got '%v'", v.success, isSuccess)
+		}
+		if v.response != "OK" && err.Error() != v.response {
+			t.Fatalf("Expected error '%v', got '%v'", v.response, err.Error())
+		}
+	}
 }
