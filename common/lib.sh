1a49341c689d6caa4be16f0bc4ea49eda1d81a4f
tamal <tamal@appscode.com>
2017-07-06 08:56:04 -0700

    Squashed 'hack/libbuild/' changes from c32c249..4cca445
    4cca445 Update libbuild.py
    447ab55 Update reimport.py
    47a901a Update reimport.py
    6b850ad Update libbuild.py
    3ae7f19 Allow reproducible build for tagged builds. (#10)
    7b8361c Update reimport list
    44e6f2e Update import list
    fd2a39a Create reimport.py
    4e1e0c2 Update reimport list
    fe1a5bb Update client-go pkg map
    64ff92c Change package imports of GO source files (#9)
    e73efa5 Treat release-* branches like master (#8)
    0ad6a1a Retry docker push for attic until successful
    99315db Delete docker image from Docker Hub for a given tag (#7)
    97a4163 Rename k8sdb to kubedb
    454a94f Support globs for ungrouping imports
    d6d6779 Avoid adding .exe when uploading file.
    9306596 Use $DOCKER_REGISTRY in build command
    f2b13fe Load lib.sh from project specific files.
    cf9de3a Introduce DOCKER_REGISTRY env variable (#4)
    a512ccd Move project specific docker methods to their own files. (#3)
    f46deb7 Add k8sdb & cowrypay scripts. (#2)
    3313b96 Skip files for update_registry
    ef11790 Add dot env for loading ENV vars (#1)
    5837743 Add default list of deps
    1e2eda3 Fix write_json.
    7fb3825 Update README.md
    765a9c7 Print message indicating current file/dir worked on.
    97853a2 Fix fn name
    06384a1 Add ungroup_go_imports
    5d6aa3f Set version linker vars only if enabled.
    
    git-subtree-dir: hack/libbuild
    git-subtree-split: 4cca4459d445d27086af5c57f619121fe02dd059

--- common/lib.sh
+++ common/lib.sh
@@ -66,7 +66,7 @@ detect_tag() {
     if [ "$git_tag" != '' ]; then
         TAG=$git_tag
         TAG_STRATEGY='git_tag'
-    elif [ "$git_branch" != 'master' ] && [ "$git_branch" != 'HEAD' ]; then
+    elif [ "$git_branch" != 'master' ] && [ "$git_branch" != 'HEAD' ] && [[ "$git_branch" != release-* ]]; then
         TAG=$git_branch
         TAG_STRATEGY='git_branch'
     else
@@ -102,49 +102,58 @@ EOL
 }
 
 build() {
-	local cmd="docker build -t appscode/$IMG:$TAG ."
+	local cmd="docker build -t $DOCKER_REGISTRY/$IMG:$TAG ."
 	echo $cmd; $cmd
 }
 
-docker_up() {
-	local cmd="docker tag appscode/$1 gcr.io/$GCR_PROJECT/$1"
+attic_up() {
+	local cmd="docker tag $DOCKER_REGISTRY/$IMG:$TAG gcr.io/$GCR_PROJECT/$IMG:$TAG"
 	echo $cmd; $cmd
-	cmd="gcloud docker -- push gcr.io/$GCR_PROJECT/$1"
+	cmd="gcloud docker -- push gcr.io/$GCR_PROJECT/$IMG:$TAG"
 	echo $cmd; $cmd
 
-	local cmd="docker tag appscode/$1 docker.appscode.com/$1"
+	local cmd="docker tag $DOCKER_REGISTRY/$IMG:$TAG docker.appscode.com/$IMG:$TAG"
 	echo $cmd; $cmd
-	cmd="docker push docker.appscode.com/$1"
+	cmd="docker push docker.appscode.com/$IMG:$TAG"
+	echo $cmd 
+	until $cmd; do echo "Try again"; done
+}
+
+hub_up() {
+	local cmd="docker push $DOCKER_REGISTRY/$IMG:$TAG"
 	echo $cmd; $cmd
 }
 
-# override this one if you need to change push
-docker_push() {
-	docker_up $IMG:$TAG
+hub_canary() {
+	hub_up
+
+	local cmd="docker tag $DOCKER_REGISTRY/$IMG:$TAG $DOCKER_REGISTRY/$IMG:canary"
+	echo $cmd; $cmd
+	cmd="docker push $DOCKER_REGISTRY/$IMG:canary"
+	echo $cmd; $cmd
 }
 
-docker_pull() {
+attic_pull() {
 	local cmd="docker pull docker.appscode.com/$IMG:$TAG"
 	echo $cmd; $cmd
-	cmd="docker tag docker.appscode.com/$IMG:$TAG appscode/$IMG:$TAG"
+	cmd="docker tag docker.appscode.com/$IMG:$TAG $DOCKER_REGISTRY/$IMG:$TAG"
 	echo $cmd; $cmd
 }
 
-docker_gcr() {
+gcr_pull() {
 	local cmd="gcloud docker -- pull gcr.io/$GCR_PROJECT/$IMG:$TAG"
 	echo $cmd; $cmd
-	cmd="docker tag gcr.io/$GCR_PROJECT/$IMG:$TAG appscode/$IMG:$TAG"
+	cmd="docker tag gcr.io/$GCR_PROJECT/$IMG:$TAG $DOCKER_REGISTRY/$IMG:$TAG"
 	echo $cmd; $cmd
 }
 
 docker_release() {
-	local cmd="docker push appscode/$IMG:$TAG"
-	echo $cmd; $cmd
+	hub_up
 }
 
 docker_check() {
 	name=$IMG-$(date +%s | sha256sum | base64 | head -c 8 ; echo)
-	local cmd="docker run -d -P -it --name=$name appscode/$IMG:$TAG"
+	local cmd="docker run -d -P -it --name=$name $DOCKER_REGISTRY/$IMG:$TAG"
 	echo $cmd; $cmd
 	cmd="docker exec -it $name ps aux"
 	echo $cmd; $cmd
@@ -168,7 +177,7 @@ docker_run() {
 	docker_cmd="${DOCKER_CMD:-}"
 	echo pv > .gitignore
 	mkdir -p pv
-	local cmd="docker run -d -P -it $privileged $net $extra_opts --name=$name appscode/$img:$TAG $docker_cmd"
+	local cmd="docker run -d -P -it $privileged $net $extra_opts --name=$name $DOCKER_REGISTRY/$img:$TAG $docker_cmd"
 	echo $cmd; $cmd
 }
 
@@ -181,7 +190,7 @@ docker_sh() {
 	privileged="${PRIVILEGED_CONTAINER:-}"
 	net="${DOCKER_NETWORK:-}"
 	extra_opts="${EXTRA_DOCKER_OPTS:-}"
-	local cmd="docker run -d -P -it $privileged $net $extra_opts --name=$name appscode/$img:$TAG"
+	local cmd="docker run -d -P -it $privileged $net $extra_opts --name=$name $DOCKER_REGISTRY/$img:$TAG"
 	echo $cmd; $cmd
 	cmd="docker exec -it $name bash"
 	echo $cmd; $cmd
